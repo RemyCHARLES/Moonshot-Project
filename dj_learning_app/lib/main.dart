e0c3b9b32efe9d93044bfeb9814b5416a87f78c4
Rémy Charles <100137905+RemyCHARLES@users.noreply.github.com>
2025-04-09 12:15:36 +0200

    setup the frontend (Flutter)

---
+++ dj_learning_app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'presentation/screens/welcome/welcome_screen.dart';
+import 'presentation/screens/login/login_screen.dart';
+import 'presentation/screens/register/register_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const WelcomeScreen(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+  ],
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ dj_learning_app/lib/presentation/screens/register/register_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Center(child: Text("Register")));
+}
